2cbc26fef405cc4a50d072c6ce0d469320dcd2b9
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2024-08-04 14:27:55 -0500

    feat: add index.html, index.js, and README

---
+++ README.md
@@ -0,0 +1,11 @@
+The purpose of this project is to exercise basic JavaScript skills aquired through The ODIN Project.
+
+Project Goals:
+- Build a rock, paper, scissors game in JavaScript from scatch.
+- Write logic to get a human and computer choice from the string values ("rock", "paper", or "scissors").
+- Declare players score variables and keep track of the score.
+- Write logic to play 5 rounds of the game entirely in the console and declare a winner at the end. 
+
+Link to the The Odin Project page:
+
+https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
---
+++ index.js
@@ -0,0 +1,63 @@
+const choices = ["rock", "scissors", "paper"];
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice () {
+    const choiceIndex = Math.floor(Math.random() * choices.length);
+    return choices[choiceIndex];
+}
+
+function getHumanChoice () {
+    let choice = prompt ('choose rock, paper, or scissors').toLowerCase();
+    if (choices.includes(choice)) {
+        return choice;
+    } else {
+        console.log("You didn't choose a valid option");
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    let = choices[0] = choices[0];
+    let = choices[1] = choices[1];
+    let = choices[2] = choices[2];
+    let = choices[0] > choices[1];
+    let = choices[1] > choices[2];
+    let = choices[2] > choices[0];
+
+    if (computerChoice > humanChoice) {
+        computerScore += 1;
+        console.log("Computer scores 1 point!");
+    } else if (computerChoice < humanChoice) {
+        humanScore += 1;
+        console.log("You scored 1 point!");
+    } else if (computerChoice === humanChoice) {
+        humanScore = humanScore;
+        computerScore = computerScore;
+        console.log("It's a draw!");
+    }       
+}
+
+playRound(humanSelection, computerSelection);
+// I do not yet understand why I have to call this function in order to have 5 rounds. 
+// Without it, the first prompt works but the round is not played. All four rounds after are then played with no issues.
+
+function playGame () {
+    for (let i = 0; i < 4; i++) {
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`Thats the end of the match!
+            Human Score: ${humanScore}
+            Computer Score: ${computerScore}`);
+}
+
+playGame();
+
+
+
+
