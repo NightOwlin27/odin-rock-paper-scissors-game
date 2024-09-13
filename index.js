73da0f8d83a0562034ae9519522b39d5c120ff18
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2024-09-13 14:06:16 -0500

    feat/fix: correct playRound function/add UI buttons
    - fixed the playRound function to correctly determine the outcome of an individual round
    - replaced the prompt with UI buttons. One for each player choice, (rock, paper, or scissors).

--- index.js
+++ index.js
@@ -1,102 +1,91 @@
+const choices = ["rock", "paper", "scissors"];
+
 const rock = document.querySelector('.btn.rock')
 const paper = document.querySelector('.btn.paper')
 const scissors = document.querySelector('.btn.scissors')
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-
-const choices = ["rock", "scissors", "paper"];
+let humanScore = 0;
+let computerScore = 0; 
+let playRoundCount = 0;
 
 rock.addEventListener('click', function () {
-    let computerChoice = getComputerChoice.value
-    humanChoice = choices[1];
-    playRound(humanCoice, computerChoice);
+    let humanChoice = choices[0];
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+    console.log(humanChoice, computerChoice)
+    playRoundCount += 1;
+    console.log(playRoundCount)
+    playGame();
 })
 
 paper.addEventListener('click', function () {
-    humanChoice = choices[1];
-    playRound(humanCoice);
+    let humanChoice = choices[1];
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+    console.log(humanChoice, computerChoice)
+    playRoundCount += 1;
+    console.log(playRoundCount)
+    playGame();
+})
+
+scissors.addEventListener('click', function () {
+    let humanChoice = choices[2];
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+    console.log(humanChoice, computerChoice)
+    playRoundCount += 1;
+    console.log(playRoundCount)
+    playGame();
 })
 
 function getComputerChoice () {
-    const choiceIndex = Math.floor(Math.random() * choices.length);
-    return choices[choiceIndex];
+    const computerChoice = Math.floor(Math.random() * choices.length);
+    return choices[computerChoice];
 }
-
-// function getHumanChoice () {
-//     rock = choices[0]
-//     paper = choices[1]
-//     scissors = choices[2]
-
-// }
-
-function playRound () {
-    computer
-    console.log(humanChoice)
-}
-
-
-// let humanScore = 0;
-// let computerScore = 0; 
-
-// function getHumanChoice () {
-//     const choices = ["rock", "scissors", "paper"];
-//     let choice = prompt ('choose rock, paper, or scissors').toLowerCase();
     
-//     if (choices.includes(choice)) {
-//         return choice;
-//     } else {
-//         console.log("You didn't choose a valid option");
-//     }
-// }
+//  I had to switch the array choices to vice versa in order to get a correct match between the
+// player and computer and I do not yet understand why.
 
- 
-// function playRound(humanChoice, computerChoice) {
-    // let = choices[0] = choices[0];
-    // let = choices[1] = choices[1];
-    // let = choices[2] = choices[2];
-    // let = choices[0] > choices[1];
-    // let = choices[1] > choices[2];
-    // let = choices[2] > choices[0];
+function playRound(humanChoice, computerChoice) {
 
-    // if (computerChoice > humanChoice) {
-    //     computerScore += 1;
-    //     console.log("Computer scores 1 point!");
-    // } else if (computerChoice < humanChoice) {
-    //     humanScore += 1;
-    //     console.log("You scored 1 point!");
-    // } else if (computerChoice === humanChoice) {
-    //     humanScore = humanScore;
-    //     computerScore = computerScore;
-    //     console.log("It's a draw!");
-    // }
-// }
+    if (humanChoice === computerChoice) {
+        humanScore = humanScore;
+        computerScore = computerScore;
+        console.log("It's a draw");
+    } else if (
+        (humanChoice === choices[0] && computerChoice === choices[2]) ||
+        (humanChoice === choices[1] && computerChoice === choices[0]) ||
+        (humanChoice === choices[2] && computerChoice === choices[1])
+    ) {
+        humanScore += 1;
+        console.log("You scored 1 point!");
+    } else {
+        computerScore += 1;
+        console.log("Computer scores 1 point!");
+    }  
+}
 
-// playRound(humanSelection, computerSelection);
-// // I do not yet understand why I have to call this function in order to have 5 rounds. 
-// // Without it, the first prompt works but the round is not played. All four rounds after are then played with no issues.
+function playGame () {
+    if (playRoundCount === 5) {
+        getFinalScore();
+    }
+}
 
-// function playGame () {
-//     for (let i = 0; i < 4; i++) {
-//         const computerSelection = getComputerChoice();
-//         const humanSelection = getHumanChoice();
-//         playRound(humanSelection, computerSelection);
-//     }
-// // Should I make this a separate function? getScore?
-//     if (humanScore > computerScore) {
-//         console.log(`Thats the end of the match! You Win!`);
-//     } else if (computerScore > humanScore) {
-//         console.log(`Thats the end of the match! You Lost!
-//                             Computer Wins!`);
-//     } else if (humanScore === computerScore) {
-//         console.log(`Thats the end of the match! It's a draw!`);
-//     }
-//         console.log(`
-//                     Human Score: ${humanScore}
-//                     Computer Score: ${computerScore}`);
-// }
+function getFinalScore () {
+    if (humanScore > computerScore) {
+        console.log(`Thats the end of the match! You Win!`);
+    } else if (computerScore > humanScore) {
+        console.log(`Thats the end of the match! You Lost!
+                                Computer Wins!`);
+    } else if (humanScore === computerScore) {
+        console.log(`Thats the end of the match! It's a draw!`);
+    }
+        console.log(`
+                Human Score: ${humanScore}
+                Computer Score: ${computerScore}`);
+    }
 
-// playGame();
+
 
 
 
