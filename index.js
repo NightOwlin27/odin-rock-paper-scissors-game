e5f2db09f9928bfe4fd0407c32762fd25a3cc343
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2024-09-22 18:33:21 -0500

    feat: add SVG icon interaction/styling changes
    - Added SVG icons to appear in .player icon and .cpu icon when either player makes their selection.

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const playerScore = document.querySelector('.player.score');
 const cpuScore = document.querySelector('.cpu.score');
 const prompt = document.querySelector(".prompt");
 
+const svgRockContainer = document.querySelector('.player.icon')
+const svgRock = `<img class="svg" src = "images/rock.svg" alt="a rock icon" width="150px" height="auto">`
+
+const svgPaperContainer = document.querySelector('.player.icon')
+const svgPaper = `<img class="svg" src = "images/paper.svg" alt="a paper icon" width="100px" height="auto">`
+
+const svgScissorsContainer = document.querySelector('.player.icon')
+const svgScissors = `<img class="svg" src = "images/scissors.svg" alt="a scissors icon" width="130px" height="auto">`
+
 let humanScore = 0;
 let computerScore = 0; 
 let playRoundCount = 0;
@@ -15,39 +24,57 @@ playerScore.textContent += humanScore;
 cpuScore.textContent += computerScore;
 
 rock.addEventListener('click', function () {
+
+   svgRockContainer.innerHTML = svgRock;
+
     let humanChoice = choices[0];
     let computerChoice = getComputerChoice()
     playRound(humanChoice, computerChoice)
-    console.log(humanChoice, computerChoice)
     playRoundCount += 1;
-    console.log(playRoundCount)
     playGame();
 })
 
 paper.addEventListener('click', function () {
+    
+    svgPaperContainer.innerHTML = svgPaper;
+
     let humanChoice = choices[1];
     let computerChoice = getComputerChoice()
     playRound(humanChoice, computerChoice)
-    console.log(humanChoice, computerChoice)
     playRoundCount += 1;
-    console.log(playRoundCount)
     playGame();
 })
 
 scissors.addEventListener('click', function () {
+    
+    svgScissorsContainer.innerHTML = svgScissors;
+
     let humanChoice = choices[2];
     let computerChoice = getComputerChoice()
     playRound(humanChoice, computerChoice)
-    console.log(humanChoice, computerChoice)
     playRoundCount += 1;
-    console.log(playRoundCount)
     playGame();
 })
 
+// Event Listener functions need to be consolidated
+
 function getComputerChoice () {
+
+    const svgRockContainer = document.querySelector('.cpu.icon')
+    const svgPaperContainer = document.querySelector('.cpu.icon')
+    const svgScissorsContainer = document.querySelector('.cpu.icon')
     const computerChoice = Math.floor(Math.random() * choices.length);
+
+    if (choices[computerChoice] === choices[0]) {
+        svgRockContainer.innerHTML = svgRock;
+    } else if (choices[computerChoice] === choices[1]) {
+        svgPaperContainer.innerHTML = svgPaper;
+    } else if (choices[computerChoice] === choices[2]) {
+        svgScissorsContainer.innerHTML = svgScissors;
+    }
     return choices[computerChoice];
 }
+
 
 function playRound(humanChoice, computerChoice) {
 
@@ -101,7 +128,13 @@ function getFinalScore () {
                 Computer Score: ${computerScore}`;
     }
 
+// function scoreReset () {
+//      humanScore = 0;
+//     computerScore = 0; 
+//     playRoundCount = 0;
+// }
 
+// Still need a working score reset function
 
 
 
